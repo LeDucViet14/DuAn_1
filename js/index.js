8c68bfb9ea806c0879716087babc2d78bb9121b5
LeDucViet14 <ducvietle1404@gmail.com>
2023-11-04 17:48:23 +0700

    trang home

---
+++ js/index.js
@@ -0,0 +1,46 @@
+var swiper = new Swiper(".swiper-container", {
+    spaceBetween: 30,
+    centeredSlides: true,
+    autoplay: {
+      delay: 4500,
+      disableOnInteraction: false,
+    },
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+    loop:true,
+});
+
+var swiper = new Swiper(".swiper-testimonials", {
+    effect: "coverflow",
+    grabCursor: true,
+    centeredSlides: true,
+    slidesPerView: "auto",
+    slidesPerView: "3",
+    loop:true,
+    coverflowEffect: {
+      rotate: 50,
+      stretch: 0,
+      depth: 100,
+      modifier: 1,
+      slideShadows: false,
+    },
+    pagination: {
+      el: ".swiper-pagination",
+    },
+    breakpionts: {
+        320:{
+            slidesPerView: 1,
+        },
+        640:{
+            slidesPerView: 1,
+        },
+        768:{
+            slidesPerView: 2,
+        },
+        1024:{
+            slidesPerView: 3,
+        },
+    }
+  });
